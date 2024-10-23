528d87704b0e809cc3fd20057b72cc9ee5ddfba7
drone117 <98345857+drone117@users.noreply.github.com>
2024-10-23 22:43:34 +0300

    Hello peeps

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func aweSomeProject() {
+	var stringToPrint string = "me make golang program"
+	fmt.Println(stringToPrint)
+	var intNum uint64 = 1
+	fmt.Println(intNum)
+}
+
+func main() {
+	aweSomeProject()
+}
